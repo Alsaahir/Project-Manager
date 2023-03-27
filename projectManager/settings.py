36e03d84b30f99098d3d26739e9e18ade3c2b4c5
Alsaahir <jacobwilsonpmwale@gmail.com>
2023-03-27 23:24:12 +0200

    update

--- projectManager/settings.py
+++ projectManager/settings.py
@@ -84,6 +84,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'OPTIONS': {
+            'version': '3.8.11.1', 
+        }
     }
 }
 
