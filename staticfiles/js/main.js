cf743bd1aef8c1ee25138d871135e1eca1f168bb
Jacob Mwale <jacobwilsonpmwale@gmail.com>
2023-05-23 00:09:25 +0200

    collected static

---
+++ staticfiles/js/main.js
@@ -0,0 +1,72 @@
+  //GET SEARCH FORM AND PAGE LINKS
+  let searchForm = document.getElementById('searchForm')
+  let pageLinks = document.getElementsByClassName('page-link')
+
+  //ENSURE SEARCH FORM EXISTS
+  if (searchForm) {
+      for (let i = 0; pageLinks.length > i; i++) {
+          pageLinks[i].addEventListener('click', function (e) {
+              e.preventDefault()
+
+              //GET THE DATA ATTRIBUTE
+              let page = this.dataset.page
+
+              //ADD HIDDEN SEARCH INPUT TO FORM
+              searchForm.innerHTML += `<input value=${page} name="page" hidden/>`
+
+
+              //SUBMIT FORM
+              searchForm.submit()
+          })
+      }
+  }
+
+// JS FOR ONLINE STATUS
+function hasNetwork(online) {
+    const element = document.querySelector(".status");
+
+    if (online) {
+        element.classList.remove("offline");
+        element.classList.add("online");
+    } else {
+        element.classList.remove("online");
+        element.classList.add("offline");
+    }
+};
+
+window.addEventListener("load", () => {
+    hasNetwork(navigator.onLine);
+
+    window.addEventListener("online", () => {
+        hasNetwork(true);
+    });
+
+    window.addEventListener("offline", () => {
+        hasNetwork(false);
+    });
+});
+
+
+// DELETE TAGS
+let tags = document.getElementsByClassName('project-tag')
+
+    for (let i=0; tags.length > i; i++) {
+        tags[i].addEventListener('click', (e) => {
+            let tagId = e.target.dataset.tag
+            let projectId = e.target.dataset.project
+
+            // console.log('TAG ID:', tagId)
+            // console.log('PROJECT ID:', projectId)
+            fetch('http://127.0.0.1:8000/api/remove-tag/', {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({'tag':tagId, 'project': projectId})
+            })
+                .then(response => response.json())
+                .then(data => {
+                    e.target.remove()
+                })
+        })
+    }
